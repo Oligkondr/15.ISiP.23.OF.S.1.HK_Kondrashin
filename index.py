af702a61f35d013becf53931f61920dba008bdf1
olegk <oligkondr@mail.ru>
2025-03-31 20:31:23 +0300

    Done first part

--- index.py
+++ index.py
@@ -11,8 +11,62 @@
 # secondNumber = int(input("Enter second number: "))
 # print(firstNumber + secondNumber)
 
-#4
-# age = input("Enter your age: ")
-# print(age.isdigit())
+# 4
+# print(input("Enter your age: ").isdigit())
 
 # 5
+# print(input("Enter a string: ").upper())
+
+# 6
+# print("hello world".split(' '))
+
+# 7
+# text = "A lazy cat sat and ate a banana."
+# print(text.count('a'))
+
+# 8
+# print(input("Enter a sentence: ").startswith('a'))
+
+# 9
+# text2 = "The sun sets, casting warm colors across the sky."
+# print(text2.replace(' ', '_'))
+
+# 10
+# words = ["apple", "book", "chair", "dog", "flower"]
+# print(", ".join(words))
+
+# 11
+# print(input("Enter a something: ").find('e'))
+
+# 12
+# text = "The sun sets behind the mountains."
+#
+# def reverse (string):
+#     return string[::-1]
+#
+# print(reverse(text))
+
+# 13
+# def converter (cel_degrees):
+#     return cel_degrees * 9 / 5 + 32
+# print(converter(36))
+
+# 14
+# user_string = input("Enter a string: ")
+# print(''.join(char for char in user_string if not char.isdigit()))
+
+# 15
+# user_string = input("Enter a string: ")
+# print(''.join(set(user_string)))
+
+# 16
+# def no_double(string):
+#     out = set()
+#     result = []
+#     for char in string:
+#         if char not in out:
+#             out.add(char)
+#             result.append(char)
+#     return ''.join(result)
+#
+# print(no_double('aaabbccddeeffgg'))
