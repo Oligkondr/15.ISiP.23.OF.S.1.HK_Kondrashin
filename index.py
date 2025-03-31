b2360784ab46303d4e9d63c515af4d9bc4af5b80
olegk <oligkondr@mail.ru>
2025-03-31 18:52:22 +0300

    Init

---
+++ index.py
@@ -0,0 +1,18 @@
+# 1
+# print(f'Hello, {input("What is your name: ")}!')
+
+# 2
+# num = 42
+# print(type(num))
+# print(type(str(num)))
+
+# 3
+# firstNumber = int(input("Enter first number: "))
+# secondNumber = int(input("Enter second number: "))
+# print(firstNumber + secondNumber)
+
+#4
+# age = input("Enter your age: ")
+# print(age.isdigit())
+
+# 5
